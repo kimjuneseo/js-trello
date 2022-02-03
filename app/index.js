47ce57d431de7d9a0e40464ae44d63a565b369b5
kimjuneseo <june3929213512@gmail.com>
2022-02-03 16:51:50 +0900

    집

--- app/index.js
+++ app/index.js
@@ -26,32 +26,28 @@ list_exitBtn.addEventListener("click", list_popupToggle);
 addlistBtn.addEventListener("click",list_popupToggle);
 card_exitBtn.addEventListener("click", card_popupToggle);
 listModify_exitBtn.addEventListener("click", listModify_popupToggle);
-
+let cardCnt = 0;
+// list Change
 const removeList = (list) => {
   list.remove();
 };
-
 const modifyPopupView = (list) => {
   listModify_popupToggle();
   let listModifyInput = listModify_listModifyForm.listModify;
   listModifyInput.value = list.childNodes[3].childNodes[1].childNodes[1].innerText;
   menuView(list);
 };
-
 const menuView = (list) =>{
   list.childNodes[1].classList.toggle('none');
 };
 
-let dataSetCnt = 0;
-let listDataSet;
-const addList = () => {
-  let title = list_listForm.list.value;
-  dataSetCnt++;
-
-  if(title !== ''){
+// Trello 
+const addList = (listDataSet, listTitle) => {
+  console.log("addList");
+  if(listTitle !== ''){
     const list = createEl('div');
     list.classList.add('list', 'grid');
-    list.dataset.list = dataSetCnt;
+    list.dataset.list = listDataSet;
     list.innerHTML += `
     <div class="list__menu--wrap none">
       <div class="modify">수정</div>
@@ -59,7 +55,7 @@ const addList = () => {
     </div>
     <div class="list__container" >
       <div class="list__header flex">
-        <p class="list__title">${title}</p>
+        <p class="list__title">${listTitle}</p>
         <div class="list__menu"><i class=" fa-ellipsis-h fa"></i></div>
       </div>
       <div class="cards grid"></div>
@@ -71,108 +67,131 @@ const addList = () => {
     addlistBtn.before(list);
     list_listForm.reset();
   };
+  
 
   boardContainer.childNodes.forEach(el => {
     el.addEventListener("click", (e) => {
         let list = document.querySelector(`.list[data-list='${e.currentTarget.dataset.list}'`);
+
         if(e.target.classList.contains("list__footer") || e.target.classList.contains("list__footer--txt") ){
-          listDataSet = e.currentTarget.dataset.list;
           card_popupWrap.classList.remove("none");
+          targetListNum = e.currentTarget.dataset.list;
         }
         if(e.target.classList.contains("fa-ellipsis-h"))menuView(list);
         if(e.target.classList.contains("modify")) modifyPopupView(list);
         if(e.target.classList.contains("remove")) removeList(list);
       });
     });
-  list_popupToggle();
-};
-
-const addCard = () => {
-  let card_input = card_cardForm.card.value;
-  if(card_input !== ''){
-    let list = document.querySelector(`.list[data-list='${listDataSet}'`);
-    list.childNodes[3].childNodes[3].innerHTML += image.src == 'http://127.0.0.1:5500/img/noimage.png' ? `<div class="card">${card_input}</div>` : `<div class="card"><img src="${image.src}" alt="card__img" id="card__add--img">${card_input}</div>`
-    image.src = '';
-    card_cardForm.reset();
-    card_popupToggle();
   }
+  
+  let targetListNum;
+  const addListListener = () => {
+    let title = list_listForm.list.value;
+    dataSetCnt++;
+    addList(dataSetCnt, title);
+    DBAdd('trello__list' ,dataSetCnt, title);
+    list_popupToggle();
+  };
+  
+
+  const addCard = (listDataSet, cardTitle, cardImg) => {
+    if(cardTitle !== ''){
+      let list = document.querySelector(`.list[data-list='${listDataSet}'`);
+      list.childNodes[3].childNodes[3].innerHTML += cardImg == 'http://127.0.0.1:5500/img/noimage.png' ? `<div class="card">${cardTitle}</div>` : `<div class="card"><img src="${cardImg}" alt="card__img" id="card__add--img">${cardTitle}</div>`
+      image.src = '';
+      card_cardForm.reset();
+      cardCnt++;
+    }
+  }
+  
+  const addCardListener = () => {
+    card_popupToggle();
+    let card_title = card_cardForm.card.value;
+    DBAdd('trello__card', targetListNum, card_title, image.src);
+    addCard(targetListNum, card_title, image.src)
+    
 };
 
 const modifyList = () => {
   listModify_popupToggle();
 };
 
-list_addBtn.addEventListener("click", addList);
-card_addBtn.addEventListener("click", addCard);
+// DB METHOD
+let db = null;
+let dataSetCnt = 0;
+const DBCreate = (dbName, dbVersion) => {
+  const request = indexedDB.open(dbName,dbVersion);
+
+      request.onupgradeneeded = e => {
+          db = e.target.result;
+          const pNotes = db.createObjectStore("trello__list", {keyPath: "dataSet"});
+          const todoNotes = db.createObjectStore("trello__card",{keyPath: "cardCnt"});
+         console.log(`upgrade is called database name: ${db.name} version : ${db.version}`);
+      }
+
+      request.onsuccess = e => {
+          db = e.target.result;
+          console.log(`success is called database name: ${db.name} version : ${db.version}`);
+          viewLists("trello__list");
+          viewLists("trello__card");
+      }
+      
+      request.onerror = e => {
+          console.log(`error: ${e.target.error} was found `);
+      }
+};
+
+const DBAdd = (tableName, dataSet, title, image) => {
+  const data = tableName === 'trello__list' ? {dataSet, title} : {cardCnt, dataSet, title, image} 
+  const tx = db.transaction(tableName, "readwrite");
+  tx.onerror = e => console.log(`Error! ${e.target.error}`);
+  const table = tx.objectStore(tableName);
+  table.add(data);
+
+};
+
+const viewLists = (name) => {
+  const tx = db.transaction(name,"readonly");
+  const pNotes = tx.objectStore(name);
+  const request = pNotes.openCursor();
+  request.onsuccess = e => {
+      const cursor = e.target.result;
+      if (cursor) {
+        if(name === 'trello__list'){
+          dataSetCnt = cursor.key;
+          addList(dataSetCnt, cursor.value.title);
+        }else{
+          cardCnt = cursor.key;
+          addCard(cursor.value.dataSet, cursor.value.title, cursor.value.image);
+        }
+        cursor.continue();
+      }
+  };
+};
+
+// init
+const init = (() => {
+  DBCreate('Trello', 1);
+})();
+
+list_addBtn.addEventListener("click", addListListener);
+card_addBtn.addEventListener("click", addCardListener);
 listModify_addBtn.addEventListener("click", modifyList);
 
+
 const openFileButton = document.querySelector("#openFile"); 
-openFileButton.addEventListener("click", async e => {
-  const [fileHandle] = await showOpenFilePicker();   
-  const file = await fileHandle.getFile();
-  const urlCreator = window.URL || window.webkitURL;
-  const mediaUrl = urlCreator.createObjectURL(file);
-  image.src = mediaUrl;
-});
+const base64File = (file) =>{
+  let reader = new FileReader();
 
-const viewNotes = () => {
+  reader.onload = function () {
+     let result = reader.result;
+     image.src = result
+     return result;
+  };
+  reader.readAsDataURL( file ); 
+};
 
-  const tx = db.transaction("personal_notes","readonly")
-  const pNotes = tx.objectStore("personal_notes")
-  const request = pNotes.openCursor()
-  request.onsuccess = e => {
+openFileButton.addEventListener("change",(e) => {
+  base64File(e.target.files[0])
+})
 
-      const cursor = e.target.result
-
-      if (cursor) {
-          alert(`Title: ${cursor.key} Text: ${cursor.value.text} `)
-          //do something with the cursor
-          cursor.continue()
-      }
-  }
-
-}
-
-const addNote = () => {
-
-  const note = {
-      title: "note" + Math.random(),
-      text: "This is my note"
-  }
-
-  const tx = db.transaction("personal_notes", "readwrite")
-  tx.onerror = e => alert( ` Error! ${e.target.error}  `)
-  const pNotes = tx.objectStore("personal_notes")
-  pNotes.add(note)
-}
-
-const createDB = (dbName, dbVersion) => {
-
-  const request = indexedDB.open(dbName,dbVersion)
-
-      //on upgrade needed
-      request.onupgradeneeded = e => {
-          db = e.target.result
-         /* note = {
-              title: "note1",
-              text: "this is a note"
-          }*/
-          const pNotes = db.createObjectStore("personal_notes", {keyPath: "title"})
-          const todoNotes = db.createObjectStore("todo_notes", {keyPath: "title"})
-         
-         alert(`upgrade is called database name: ${db.name} version : ${db.version}`)
-
-      }
-      //on success 
-      request.onsuccess = e => {
-          db = e.target.result
-          alert(`success is called database name: ${db.name} version : ${db.version}`)
-      }
-      //on error
-      request.onerror = e => {
-          alert(`error: ${e.target.error} was found `)
-           
-      }
-
-
-}
